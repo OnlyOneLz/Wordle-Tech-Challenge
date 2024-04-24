1ea4da1974da3594bd61eb7f350ef4180dbc1282
OnlyOneLz <elliotrobertlewis@hotmail.com>
2024-04-24 15:57:18 +0100

    grid created

--- client/src/script.js
+++ client/src/script.js
@@ -6,14 +6,33 @@ const startBtn = document.querySelector('.load-page-button')
 const loadPageModal = document.querySelector('.load-page')
 const homePage = document.querySelector('.home-page')
 const homeContainer = document.querySelector('.home-container')
+const gridContainer = document.querySelector('.grid-container');
+
+// Variables
+
+const numRows = 6;
+const numCols = 5;
 
 // Functions
+
+const createWordleGrid = () => {
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+          const gridItem = document.createElement('div');
+          gridItem.classList.add('grid-item');
+          gridContainer.appendChild(gridItem);
+        }
+      }
+}
 
 const startGame = () => {
     loadPageModal.style.display = 'none'
     homePage.style.backgroundColor = 'rgba(227, 219, 219, 1)'
     homeContainer.style.display = 'flex'
+    createWordleGrid()
 }
+
+
 
 // Event Listenrs
 
